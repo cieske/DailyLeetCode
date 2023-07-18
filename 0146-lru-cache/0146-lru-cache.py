f6f9c864e50cb4192ef81624398c8b6c73a7919a
cieske <36599490+cieske@users.noreply.github.com>
2023-07-18 23:18:04 +0900

    Time: 714 ms (86.14%), Space: 77.4 MB (89.34%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,62 +1,29 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        from collections import defaultdict
+        from collections import OrderedDict
         
         # Used for key-value matching
         # Save value as value+1, 0 for null checking
-        self.dic = defaultdict(int) 
-        self.dll = [[None, None] for _ in range(10005)]
-        self.head = None
-        self.tail = None
+        self.dic = OrderedDict()
         self.capa = capacity
-        self.num = 0
         
 
     def get(self, key: int) -> int:
-        if self.dic[key]:
-            self.update(key)
-        return self.dic[key] - 1
+        if key in self.dic:
+            self.dic.move_to_end(key)
+            return self.dic[key]
+        else:
+            return -1
             
 
     def put(self, key: int, value: int) -> None:
-        if self.dic[key]: # existing key
-            self.update(key=key)
-        elif self.head == None: # first put
-            self.num += 1
-            self.head, self.tail = key, key
-        else: # new key
-            self.num += 1
-            self.to_head(key)
-            
-            if self.num > self.capa: # evict
-                self.dic[self.tail] = 0
-                self.tail = self.dll[self.tail][0]
-                self.dll[self.tail][1] = None              
-
-        self.dic[key] = value + 1
-    
-    
-    def update(self, key):
-        if key == self.head:
-            return
-        elif key == self.tail:
-            self.tail = self.dll[key][0]
-            self.dll[self.tail][1] = None
-            self.to_head(key)
-        else:
-            k_head, k_tail = self.dll[key][0], self.dll[key][1]
-            # if k_tail:
-            self.dll[k_tail][0] = k_head
-            # if k_head:
-            self.dll[k_head][1] = k_tail                
-            self.to_head(key)
+        if key in self.dic:
+            self.dic.move_to_end(key)
+        self.dic[key] = value
         
-        
-    def to_head(self, key):
-        self.dll[self.head][0] = key
-        self.dll[key] = [None, self.head]
-        self.head = key
+        if len(self.dic) > self.capa:
+            self.dic.popitem(last=False)
         
 
 # Your LRUCache object will be instantiated and called as such:
