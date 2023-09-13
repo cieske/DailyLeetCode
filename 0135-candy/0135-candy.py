1270c9363aa2cbc5740ce4821b2af98357b951fe
cieske <36599490+cieske@users.noreply.github.com>
2023-09-13 12:12:29 +0900

    Time: 143 ms (67.56%), Space: 19.3 MB (68.60%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,13 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        sol = [1]*len(ratings)
+        
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                sol[i] = sol[i-1] + 1
+                
+        for i in range(len(ratings)-1, 0, -1):
+            if ratings[i-1] > ratings[i]:
+                sol[i-1] = max(sol[i-1], sol[i]+1)
+                
+        return sum(sol)
