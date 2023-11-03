eeddcd408481aff340bf98e841bc0e82241b7a08
cieske <36599490+cieske@users.noreply.github.com>
2023-11-03 12:51:23 +0900

    Time: 39 ms (65.86%), Space: 16.3 MB (44.85%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,13 +1,3 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        n = target[-1]
-        sol = []
-        last = 0
-        for num in target:
-            for _ in range(num-last-1):
-                sol.extend(['Push', 'Pop'])
-            sol.append('Push')
-            last = num
-        
-        return sol
-            
+        return ''.join([' Push Pop'*(([0]+target)[i] - ([0]+target)[i-1] - 1) + ' Push' for i in range(1, len(target)+1)]).strip().split(" ")
